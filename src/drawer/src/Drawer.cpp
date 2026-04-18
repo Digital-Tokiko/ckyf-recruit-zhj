7a831b116c7257de4794c56ee606ba6828e3bbc6
Digital_Tokiko <huijunzan@nuaa.edu.cn>
2026-04-18 15:04:56 +0800

    功能相对完备的初版，参数欠调节

---
+++ src/drawer/src/Drawer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by mizu on 2026/4/18.
+//
+
+#include "Drawer.h"
---
+++ src/drawer/src/Drawer.h
@@ -0,0 +1,13 @@
+//
+// Created by mizu on 2026/4/18.
+//
+
+#ifndef RECRUIT_DRAWER_H
+#define RECRUIT_DRAWER_H
+
+
+class Drawer {
+};
+
+
+#endif //RECRUIT_DRAWER_H
